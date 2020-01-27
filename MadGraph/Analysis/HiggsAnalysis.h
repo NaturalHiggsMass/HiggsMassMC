b04ff795aedddd7566f9bc519136752623ded7f5
Anna <kropiv@cern.ch>
2020-01-27 10:04:51 +0100

    add to run program

--- MadGraph/Analysis/HiggsAnalysis.h
+++ MadGraph/Analysis/HiggsAnalysis.h
@@ -39,17 +39,17 @@ public :
 
 // Fixed size dimensions of array or collections stored in the TTree if any.
    static constexpr Int_t kMaxEvent = 1;
-   static constexpr Int_t kMaxParticle = 4605;//4346;//4231;
-   static constexpr Int_t kMaxTrack = 257;
-   static constexpr Int_t kMaxTower = 447;//433;//349;
-   static constexpr Int_t kMaxEFlowTrack = 257;
-   static constexpr Int_t kMaxEFlowPhoton = 236;//222;//147;
-   static constexpr Int_t kMaxEFlowNeutralHadron = 190;//182;//173;
+   static constexpr Int_t kMaxParticle = 4700;//4346;//4231;
+   static constexpr Int_t kMaxTrack = 300;//257;
+   static constexpr Int_t kMaxTower = 600;//447;//433;//349;
+   static constexpr Int_t kMaxEFlowTrack = 300;//257;
+   static constexpr Int_t kMaxEFlowPhoton = 300;//236;//222;//147;
+   static constexpr Int_t kMaxEFlowNeutralHadron = 200;//190;//182;//173;
    static constexpr Int_t kMaxGenJet = 16;
    static constexpr Int_t kMaxGenMissingET = 1;
    static constexpr Int_t kMaxJet = 16;
    static constexpr Int_t kMaxElectron = 4;
-   static constexpr Int_t kMaxPhoton = 4;
+   static constexpr Int_t kMaxPhoton = 10;//4;
    static constexpr Int_t kMaxMuon = 4;
    static constexpr Int_t kMaxFatJet = 6;
    static constexpr Int_t kMaxMissingET = 1;
@@ -787,7 +787,11 @@ HiggsAnalysis::HiggsAnalysis(TTree *tree) : fChain(0)
       chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run05_Higgs0or1Jet_PtGen_200_550_forPtReco350_450GeV.root/Delphes");
       chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run06_Higgs0or1Jet_PtGen_300_650_forPtReco450_550GeV.root/Delphes");
       chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run07_Higgs0or1Jet_PtGen_380_900_forPtReco550_750GeV.root/Delphes");
-      chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run08_Higgs0or1Jet_PtGen_580_inf_forPtReco750_infGeV.root/Delphes");
+      //chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run08_Higgs0or1Jet_PtGen_580_inf_forPtReco750_infGeV.root/Delphes");
+      //test photon Iso 0.2
+      //chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/Higgs_0or1Jet_PtCut_CMSmodTestPhotonIso/run_24/run08_Higgs0or1Jet_PtGen_580_inf_forPtReco750_infGeV.root/Delphes");
+      //test photon Iso 0.3, no photo in Iso, eta dep. resolution
+      chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/Higgs_0or1Jet_PtCut_CMSmodTestPhotonIso/run_27/run08_Higgs0or1Jet_PtGen_580_inf_forPtReco750_infGeV.root/Delphes");
       // for Singal Mass 123 GeV:
       //chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run01_Higgs0or1JetMass123_PtGen_0_inf_forPtReco0_80_120GeV.root/Delphes");
       //chain->Add("/eos/cms/store/user/kropiv/HiggsMass/HiggsGG/root-files/run02_Higgs0or1JetMass123_PtGen_40_250_forPtReco120_200GeV.root/Delphes");
